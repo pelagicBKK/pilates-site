cb388362693075f458a0d3de93d7609c1bc94e33
sreaves <shannonreaves.slp@gmail.com>
2022-04-11 15:37:03 -0500

    styling homepage-lottie

--- src/components/home/TopList.jsx
+++ src/components/home/TopList.jsx
@@ -1,120 +1,143 @@
 import React, { Fragment } from 'react';
-
+import Lottie from 'react-lottie';
+import animationData from '../../images/lotties/pilates-animation.json';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Grid, Card, ListItem } from '@material-ui/core';
+// import { Grid, ListItem } from '@material-ui/core';
 
 
 
-import { Grid, ListItem } from '@material-ui/core';
 
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 
 export default function LivePreviewExample() {
   return (
     <Fragment>
-      <div style={{ marginRight:"100px", marginLeft:"100px",  }}>
+    <div style={{ marginRight:"100px", marginLeft:"100px",  }}>
             <Grid container spacing={6}>
               <Grid item xs={12} lg={6}>
-        
-              <div className="scroll-area rounded  shadow-overflow" style={{width:"100%", marginLeft:"auto", marginRight:"auto" }}>
+              <div className="py-0 py-xl-5" >
+              
+              <div className="pl-0 pl-xl-5 text-center text-black" >
+                    <h1 className="display-5 mb-3 font-weight-bold">
+                     Pilates
+                    </h1>
+                    </div>
+              
+              
+              
+              <ListItem className="py-3 border-0 " >
+                 
+                 <div className="d-flex align-items-center w-100">
+                 
+                   <div className="mr-3 "style={{paddingRight:25}}>
+                   {/* <Card className="card-box  mb-4  " style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
+                     <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
+                       <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
+                      
+                     </div>
+                     </Card> */}
+                   </div>
+                   
+                   <div>
+                     <div className="font-weight-bold d-block opacity-8 mb-2">
+                       <h5>
+                       Men and Women
+                       </h5>
+                     </div>
+                     <div className="text-dark opacity-5">
+                       Pilates allows for a safer and low impact exercise 
+                       to gently get your body in better shape. It also allows
+                       for stronger bones, posture and a stronger core. 
+                     </div>
+                   </div>
+                 </div>
+                
+               </ListItem>
               
 
                
-                
-                  <ListItem className="py-3 border-0 " >
-                 
-                    <div className="d-flex align-items-center w-100">
-                    
-                      <div className="mr-3 "style={{paddingRight:25}}>
-                      {/* <Card className="card-box  mb-4  " style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
-                        <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
-                          <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
-                         
-                        </div>
-                        </Card> */}
-                      </div>
-                      
-                      <div>
-                        <div className="font-weight-bold d-block opacity-8 mb-2">
-                          <h5>
-                          Men and Women
-                          </h5>
-                        </div>
-                        <div className="text-dark opacity-5">
-                          Pilates allows for a safer and low impact exercise 
-                          to gently get your body in better shape. It also allows
-                          for stronger bones, posture and a stronger core. 
-                        </div>
-                      </div>
-                    </div>
-                   
-                  </ListItem>
-                 
+               <ListItem className="py-3 border-0">
 
-                  
-                  <ListItem className="py-3 border-0">
-   
-                    <div className="d-flex align-items-center w-100">
-                    
-                      <div className="mr-3 "style={{paddingRight:25}}>
-                      {/* <Card className="card-box  mb-4 rounded " style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
-                        <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
-                          <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
-                        </div>
-                        </Card> */}
-                      </div>
-                      
-                      <div>
-                        <div className="font-weight-bold d-block opacity-8 mb-2">
-                          <h5>
-                          Athletes
-                          </h5>
-                        </div>
-                        <div className="text-dark opacity-5">
-                          Pilates will give athletes many advantages to help aid
-                          muscle recovery, endurance, strength, mind-body connection, 
-                          overall endurance and core strength and stability. 
-                        </div>
-                      </div>
-                    </div>
+                 <div className="d-flex align-items-center w-100">
+                 
+                   <div className="mr-3 "style={{paddingRight:25}}>
+                   {/* <Card className="card-box  mb-4 rounded " style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
+                     <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
+                       <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
+                     </div>
+                     </Card> */}
+                   </div>
                    
-                  </ListItem>
+                   <div>
+                     <div className="font-weight-bold d-block opacity-8 mb-2">
+                       <h5>
+                       Athletes
+                       </h5>
+                     </div>
+                     <div className="text-dark opacity-5">
+                       Pilates will give athletes many advantages to help aid
+                       muscle recovery, endurance, strength, mind-body connection, 
+                       overall endurance and core strength and stability. 
+                     </div>
+                   </div>
+                 </div>
+                
+               </ListItem>
+              
+               <ListItem className="py-3 border-0">
+            
+                 <div className="d-flex align-items-center w-100">
                  
-                  <ListItem className="py-3 border-0">
-               
-                    <div className="d-flex align-items-center w-100">
-                    
-                      <div className="mr-3 "style={{paddingRight:25}}>
-                      {/* <Card className="card-box  mb-4" style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
-                        <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
-                          <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
-                        </div>
-                        </Card> */}
-                      </div>
-                      
-                      <div>
-                        <div className="font-weight-bold d-block opacity-8 mb-2">
-                          <h5>
-                          Everyone
-                          </h5>
-                        </div>
-                        <div className="text-dark opacity-5">
-                         Pilates can be very low impact or intense which allows 
-                         for anyone to reap the benefits. 
-                         Whether working on just breathing for emotional well-being
-                         or stronger and stable muscles, Pilates has something for everyone. 
-                        </div>
-                      </div>
-                    </div>
-                 
-                  </ListItem>
-               
+                   <div className="mr-3 "style={{paddingRight:25}}>
+                   {/* <Card className="card-box  mb-4" style={{width:50, height:50, backgroundColor:"#f5f5f5"}}>
+                     <div className="bg-neutral-dark text-primary text-center font-size-xl d-60 rounded-sm">
+                       <FontAwesomeIcon style={{marginTop:20,width:20, height:20, color:"#98d2f9"}} icon={['far', 'heart']} />
+                     </div>
+                     </Card> */}
+                   </div>
+                   
+                   <div>
+                     <div className="font-weight-bold d-block opacity-8 mb-2">
+                       <h5>
+                       Everyone
+                       </h5>
+                     </div>
+                     <div className="text-dark opacity-5">
+                      Pilates can be very low impact or intense which allows 
+                      for anyone to reap the benefits. 
+                      Whether working on just breathing for emotional well-being
+                      or stronger and stable muscles, Pilates has something for everyone. 
+                     </div>
+                   </div>
+                 </div>
+              
+               </ListItem>
+            
+                
               </div>
-           
-         
-        </Grid>
-      
-      </Grid>
-      </div>
+              </Grid>
+              <Grid item xs={12} lg={6} className="d-flex align-items-center">
+                
+                <Lottie 
+                  
+                  options={defaultOptions}
+                    height={500}
+                    width={650}
+                  />
+               
+              </Grid>
+            </Grid>
+          </div>
     </Fragment>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import "./Home.css";
 import SectionOne from "../components/home/SectionOne";
-import Lottie from 'react-lottie';
-import animationData from '../images/lotties/pilates-animation.json';
+// import Lottie from 'react-lottie';
+// import animationData from '../images/lotties/pilates-animation.json';
 import TopList from "../components/home/TopList";
 // import BenefitsTable from "../components/home/BenefitsTable";
 // import stock2 from "../images/avatars/avatar2.jpg";
@@ -17,26 +17,13 @@ import {
 // import svgImage2 from "../images/illustrations/pilates-reformer-silhouette-by-Vexels.svg";
 // import hero6 from "../images/hero-bg/hero-6.jpg";
 
-export default function Home(props) {
-  // const pushSchedule = () => {
-  //  props.history.push("https://calendly.com/booking-hero/30min");
-  // };
-  // const pushSchedule = () => {
-  //   props.history.push("/about");
-  // };
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+export default function Home() {
+ 
   return (
 
     <Fragment>
-      <div className="hero-wrapper bg-composed-wrapper ">
-        <div className="flex-grow-1 w-100 d-flex align-items-center">
+       <div className="hero-wrapper">
+        <div className="flex-grow-5 d-flex align-items-center">
           <div
             // className="bg-composed-wrapper--image bg-composed-filter-rm opacity-9"
             style={{
@@ -52,185 +39,9 @@ export default function Home(props) {
             }}
        >
           <div className="bg-composed-wrapper--content py-5 ">
-            <Container className="py-5 " >
-              <Grid container spacing={4}>
-                <Grid item xs={12} lg={6}>
-                  <div className="d-flex align-items-center">
-                    <span className="px-4 h-auto py-1 badge badge-success badge-pill">
-                      {/* New version */}
-                    </span>
-                    {/* <Tooltip
-                      arrow
-                      placement="right"
-                      title="More info placeholder!"
-                    >
-                      <span className="text-black-50 pl-3">
-                        <FontAwesomeIcon icon={["far", "question-circle"]} />
-                      </span>
-                    </Tooltip> */}
-                  </div>
-                  <div className="text-black mt-3">
-                    <h1 className="mb-4  text-center">
-                      Pilates    
-                      {/* <span className="text-black-50 pl-3">
-                    <i class="fas fa-question"></i>
-                      </span> */}
-                      
-                    </h1>
-                   
-                    <div >
-                      <TopList/>
-                    {/* <blockquote className="blockquote ">
-                      <p className="font-size-lg ">
-                          Gay is a long term resident of Brentwood, TN and has an
-                        extensive background in healthcare and building
-                        relationships.  Gay is a long term resident of Brentwood, TN and has an
-                        extensive background in healthcare and building
-                        relationships.
-                      </p>
-                      </blockquote>
-                      <blockquote className="blockquote mb-0">
-                      <p className="font-size-lg ">
-                          Gay is a long term resident of Brentwood, TN and has an
-                        extensive background in healthcare and building
-                        relationships.  Gay is a long term resident of Brentwood, TN and has an
-                        extensive background in healthcare and building
-                        relationships.
-                      </p>
-                      </blockquote> */}
-                   
-                    </div>
-                    <div className="divider border-2 border-dark my-5 border-light opacity-2 rounded-circle w-25" />
-
-                    <div>
-                   
-                      {/* <Button
-                        size="large"
-                        variant="contained"
-                        color="primary"
-                        // onClick={() => this.pushSchedule({ })}
-                        // onClick={pushSchedule}
-                        className="mr-3"
-                      >
-                        <a
-                          href="https://calendly.com/booking-hero/30min"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="btn-wrapper--label">Schedule</span>
-                          <span className="btn-wrapper--icon">
-                            <FontAwesomeIcon icon={["fas", "arrow-right"]} />
-                          </span>
-                        </a>
-                      </Button>
-                      <Button size="large" variant="outlined" color="primary">
-                        <a href="mailto:abc@example.com?subject = Feedback&body = Message">
-                          Send Feedback
-                        </a>
-                      </Button> */}
-                      
-                    </div>
-                    
-                  </div>
-                </Grid>
-                
-                <Grid
-                  item
-                  xs={12}
-                  lg={6}
-                  className="px-0 d-none d-md-flex align-items-center">
-                  
-               
-                  {/* <img
-                    alt="..."
-                    className="w-100 mx-auto d-block img-fluid"
-                    src={svgImage2}
-                  /> */}
-                 <Lottie 
-                  
-	    options={defaultOptions}
-        height={500}
-        width={650}
-      />
-                </Grid> 
-              </Grid>
-            </Container>
+            <TopList/>
          </div>
-        {/* <div className=" pt-5">  */}
-         {/* <Container className="card-container">
-            <Grid container spacing={4}>
-              <Grid item xs={12} md={4}>
-                <Card className="card mb-4 shadow-lg p-3 mb-5 bg-white rounded">
-                  <CardContent className="p-3">
-                    <div className="bg-deep-blue text-center text-white display-4 d-60 rounded-circle">
-                     <AddCircleTwoToneIcon /> 
-                    </div>
-                    <h3 className="heading-6 mt-4 mb-3 font-weight-bold">
-                      Lightweight
-                    </h3>
-                    <p className="card-text mb-3">
-                      These matters to this principle of selection: he rejects
-                      pleasures to secure other.
-                    </p>
-                    <Button
-                      color="primary"
-                      variant="outlined"
-                      title="Learn more"
-                    >
-                      <span>Learn more</span>
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={4}>
-              <Card className="card mb-4 shadow-lg p-3 mb-5 bg-white rounded">
-                  <CardContent className="p-3">
-                    <div className="bg-sunny-morning text-center text-white display-4 d-60 rounded-circle">
-                      <FontAwesomeIcon icon={["fas", "lemon"]} />
-                    </div>
-                    <h3 className="heading-6 mt-4 mb-3 font-weight-bold">
-                      Simple to use
-                    </h3>
-                    <p className="card-text mb-3">
-                      Greater pleasures, or else he endures pains to avoid worse
-                      pains. But I must explain.
-                    </p>
-                    <Button
-                      color="primary"
-                      variant="outlined"
-                      title="Learn more"
-                    >
-                      <span>Learn more</span>
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={4}>
-              <Card className="card mb-4 shadow-lg p-3 mb-5 bg-white rounded" >
-                  <CardContent className="p-3">
-                    <div className="bg-grow-early text-center text-white display-4 d-60 rounded-circle">
-                      <FontAwesomeIcon icon={["far", "keyboard"]} />
-                    </div>
-                    <h3 className="heading-6 mt-4 mb-3 font-weight-bold">
-                      Starter Templates
-                    </h3>
-                    <p className="card-text mb-3">
-                      To you how all this mistaken idea of denouncing pleasure
-                      and praising.
-                    </p>
-                    <Button
-                      color="primary"
-                      variant="outlined"
-                      title="Learn more"
-                    >
-                      <span>Learn more</span>
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-                    </Container>*/}
-        {/* </div> */}
+    
         </div>
         </div>
 
